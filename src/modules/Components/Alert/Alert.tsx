34ba485d4b11a4c9a77b5d97f36f914b8f275aae
Marcin Gajda <mecenasy@gmail.com>
2021-05-13 14:07:52 +0200

    Add Input andAlert

---
+++ src/modules/Components/Alert/Alert.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import * as P from './parts';
+import { AlertType } from './types';
+
+interface AlertProps {
+   className?: string
+   message: string;
+   type: AlertType;
+}
+
+const Alert: FC<AlertProps> = ({
+   className,
+   message,
+   type,
+}) => {
+   return (
+      <P.AlertWrapper className={className} alertType={type}>
+         {message}
+      </P.AlertWrapper>
+   )
+};
+
+export default Alert
+   ;
---
+++ src/modules/Components/Alert/parts.tsx
@@ -0,0 +1,35 @@
+import styled, { css } from 'styled-components';
+import { AlertType } from './types';
+
+export const AlertWrapper = styled.div<{ alertType: AlertType }>`
+   border-radius: 4px;
+   padding: 8px;
+   font-size: 14px;
+   font-weight: 500;
+   color: black;
+   width: 100%;
+   margin: 4px;
+   
+   ${({ alertType }) => {
+      switch (alertType) {
+         case AlertType.error: {
+            return css`
+             border: 1px solid red;
+             background: #fbe4e4;
+            `;
+         }
+         case AlertType.info: {
+            return css`
+            border: 1px solid blue;
+            background: #dedeff;
+            `;
+         }
+         default: {
+            return css`
+            border: 1px solid yellow;
+            background: #f7f7e2;
+            `;
+         }
+      }
+   }}
+`;
