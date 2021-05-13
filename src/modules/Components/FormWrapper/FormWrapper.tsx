314e1dcf50f189d7806a95d423329b21a6bdeaa9
Marcin Gajda <mecenasy@gmail.com>
2021-05-13 14:06:16 +0200

    Add FormWrapper

---
+++ src/modules/Components/FormWrapper/FormWrapper.tsx
@@ -0,0 +1,63 @@
+import React, { PropsWithChildren } from 'react';
+import { AnyAction } from 'redux';
+import { Form, FormProps } from 'react-final-form';
+import MakeAsyncFunction from 'react-redux-promise-listener';
+import { promiseListener } from '~/src/store/configuration/reduxPromiseListener';
+import { handleAsyncFormSubmit } from '~/utils/handleSubmitForm';
+import { Config } from 'final-form';
+import * as P from './parts';
+
+export type SetPayload<A = AnyAction, T = Record<string, any>> = (action: A, formData: T) => A
+export type GetError<A = AnyAction> = (action: A) => void
+export type GetPayload<A = AnyAction> = (action: A) => Record<string, string> | undefined
+
+interface FormWrapperProps<A, T> extends Omit<FormProps, 'onSubmit'> {
+   className?: string;
+   start: string;
+   resolve: string;
+   reject: string;
+   setPayload?: SetPayload<A, T>;
+   getPayload?: GetPayload<A>;
+   getError?: GetError<A>;
+}
+
+type FormWrapperType = <Action = AnyAction, FormValues = Record<string, any>>(
+   props: PropsWithChildren<FormWrapperProps<Action, FormValues>>
+) => React.ReactElement;
+
+const FormWrapper: FormWrapperType = ({
+   className,
+   start,
+   resolve,
+   reject,
+   setPayload,
+   getPayload,
+   getError,
+   children,
+   ...rest
+}) => (
+   <MakeAsyncFunction
+      listener={promiseListener}
+      start={start}
+      resolve={resolve}
+      reject={reject}
+      setPayload={setPayload}
+      getPayload={getPayload}
+      getError={getError}
+   >
+      {(handleSubmit: Config['onSubmit']) => (
+         <Form
+            {...rest}
+            onSubmit={handleAsyncFormSubmit(handleSubmit)}
+         >
+            {({ handleSubmit }) => (
+               <P.FormWrapper className={className} onSubmit={handleSubmit}>
+                  {children}
+               </P.FormWrapper>
+            )}
+         </Form>
+      )}
+   </MakeAsyncFunction>
+);
+
+export default FormWrapper;
---
+++ src/modules/Components/FormWrapper/parts.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const FormWrapper = styled.form`
+  width: calc(100% - 80px);
+`;
